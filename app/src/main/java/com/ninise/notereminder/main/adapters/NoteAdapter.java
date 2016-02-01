f8e3155c4bff3149b262998bdcc3b759b6268217
Ninise <nikitonze@hotmail.com>
2016-02-01 17:11:34 +0200

    Replaced alarm button from cardview to NoteActivity

--- app/src/main/java/com/ninise/notereminder/main/adapters/NoteAdapter.java
+++ app/src/main/java/com/ninise/notereminder/main/adapters/NoteAdapter.java
@@ -2,10 +2,8 @@ package com.ninise.notereminder.main.adapters;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.Display;
@@ -15,22 +13,16 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
 import com.ninise.notereminder.R;
 import com.ninise.notereminder.Utils.Constants;
 import com.ninise.notereminder.database.NoteModel;
 import com.ninise.notereminder.database.NoteWorker;
 import com.ninise.notereminder.notedata.NoteActivity;
-import com.ninise.notereminder.notification.AlarmNotification;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
@@ -40,13 +32,11 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
     final Context context;
     NoteWorker noteWorker;
     final SimpleDateFormat date;
-    long time;
 
     public class ViewHolder extends RecyclerView.ViewHolder  {
         final TextView mTitleTextView;
         final CardView cv;
         final ImageButton mDeleteBtn;
-        final ImageButton mAlarmBtn;
         final TextView mToTextView;
 
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
@@ -63,8 +53,6 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
             mTitleTextView.setMaxWidth((int) disp_width);
 
             mDeleteBtn = (ImageButton) itemView.findViewById(R.id.dataBtnDelete);
-
-            mAlarmBtn = (ImageButton) itemView.findViewById(R.id.dataBtnSetAlarm);
 
             mToTextView = (TextView) itemView.findViewById(R.id.data_to_time);
 
@@ -117,14 +105,6 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
             }
         });
 
-        /** Click on "alarm" button for set note as alarm  */
-        holder.mAlarmBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDialog(holder.getAdapterPosition());
-            }
-        });
-
         animate(holder);
     }
 
@@ -153,23 +133,6 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
         notifyItemRemoved(position);
     }
 
-    private void update(final int position) {
-        noteWorker = new NoteWorker(context);
-
-        final NoteModel newNote = mDataSet.get(position);
-        newNote.setTime(time);
-
-        noteWorker.updateNote(newNote);
-
-        alarm(newNote);
-
-        mDataSet.remove(position);
-
-        noteWorker.close();
-
-        notifyDataSetChanged();
-    }
-
     private void sendData(final int position) {
         final Intent intent = new Intent(context, NoteActivity.class);
 
@@ -182,47 +145,5 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
         intent.putExtras(bundle);
 
         context.startActivity(intent);
-    }
-
-    @SuppressLint("InflateParams")
-    private void showDialog(final int position) {
-        final LayoutInflater factory = LayoutInflater.from(context);
-        final View dateAndTimePicker = factory.inflate(R.layout.date_time_picker, null);
-
-        final AlertDialog.Builder builder = new AlertDialog.Builder(context)
-                .setView(dateAndTimePicker)
-                .setCancelable(true)
-                .setPositiveButton(R.string.on_btn, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        DatePicker datePicker = (DatePicker) dateAndTimePicker.findViewById(R.id.date_picker);
-                        TimePicker timePicker = (TimePicker) dateAndTimePicker.findViewById(R.id.time_picker);
-
-                        Calendar calendar = new GregorianCalendar(datePicker.getYear(),
-                                datePicker.getMonth(),
-                                datePicker.getDayOfMonth(),
-                                timePicker.getCurrentHour(),
-                                timePicker.getCurrentMinute());
-
-                        time = calendar.getTimeInMillis();
-
-                        update(position);
-                    }
-                });
-
-        final AlertDialog alert = builder.create();
-        alert.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override
-            public void onShow(DialogInterface dialog) {
-                Button positiveBtn = alert.getButton(DialogInterface.BUTTON_POSITIVE);
-                positiveBtn.setTextSize(30);
-            }
-        });
-        alert.show();
-    }
-
-    private void alarm(NoteModel noteModel) {
-        final AlarmNotification alarm = new AlarmNotification(context);
-        alarm.setOnceAlarm(time, noteModel.getDescription());
     }
 }
--- app/src/main/java/com/ninise/notereminder/notedata/NoteFragment.java
+++ app/src/main/java/com/ninise/notereminder/notedata/NoteFragment.java
@@ -1,18 +1,27 @@
 package com.ninise.notereminder.notedata;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import com.ninise.notereminder.R;
 import com.ninise.notereminder.Utils.Constants;
 import com.ninise.notereminder.database.NoteModel;
 import com.ninise.notereminder.database.NoteWorker;
+import com.ninise.notereminder.notification.AlarmNotification;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class NoteFragment extends Fragment {
 
@@ -52,6 +61,15 @@ public class NoteFragment extends Fragment {
             mDescriptionEditText.setText(descript);
         }
 
+        final Button mSetTimeBtn = (Button) v.findViewById(R.id.setAlarmBtn);
+
+        mSetTimeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+
         final Button mSaveBtn = (Button) v.findViewById(R.id.saveNoteBtn);
 
         mSaveBtn.setOnClickListener(new View.OnClickListener() {
@@ -90,5 +108,46 @@ public class NoteFragment extends Fragment {
         super.onPause();
         TIME = 0;
         noteWorker.close();
+    }
+
+    @SuppressLint("InflateParams")
+    private void showDialog() {
+        final LayoutInflater factory = LayoutInflater.from(getActivity());
+        final View dateAndTimePicker = factory.inflate(R.layout.date_time_picker, null);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                .setView(dateAndTimePicker)
+                .setCancelable(true)
+                .setPositiveButton(R.string.on_btn, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DatePicker datePicker = (DatePicker) dateAndTimePicker.findViewById(R.id.date_picker);
+                        TimePicker timePicker = (TimePicker) dateAndTimePicker.findViewById(R.id.time_picker);
+
+                        Calendar calendar = new GregorianCalendar(datePicker.getYear(),
+                                datePicker.getMonth(),
+                                datePicker.getDayOfMonth(),
+                                timePicker.getCurrentHour(),
+                                timePicker.getCurrentMinute());
+
+                        TIME = calendar.getTimeInMillis();
+                        alarm(getNote());
+                    }
+                });
+
+        final AlertDialog alert = builder.create();
+        alert.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Button positiveBtn = alert.getButton(DialogInterface.BUTTON_POSITIVE);
+                positiveBtn.setTextSize(30);
+            }
+        });
+        alert.show();
+    }
+
+    private void alarm(NoteModel noteModel) {
+        final AlarmNotification alarm = new AlarmNotification(getActivity());
+        alarm.setOnceAlarm(TIME, noteModel.getDescription());
     }
 }
