dd3aee186d82ade9580a881d42ed499060b2dbbe
Ninise <nikitonze@hotmail.com>
2016-02-06 20:14:22 +0200

    Added method for generate request code for note

--- app/src/main/java/com/ninise/notereminder/utils/Utils.java
+++ app/src/main/java/com/ninise/notereminder/utils/Utils.java
@@ -3,10 +3,16 @@ package com.ninise.notereminder.utils;
 
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class Utils {
 
     public static boolean isTextViewEmpty(TextView tv) {
         return tv.getText().toString().equals("");
+    }
+
+    public static int generateRequest() {
+        return new Random().nextInt(10000) - 1;
     }
 
 }
