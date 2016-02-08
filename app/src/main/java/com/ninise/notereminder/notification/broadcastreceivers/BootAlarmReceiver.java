581ff171e190a9e70ca0bd3678e29564eb0d78ab
Ninise <nikitonze@hotmail.com>
2016-02-08 20:16:34 +0200

    Added WakefulBroadcastReceiver for start IntentService when device was rebooted

--- app/src/main/java/com/ninise/notereminder/notification/broadcastreceivers/BootAlarmReceiver.java
+++ app/src/main/java/com/ninise/notereminder/notification/broadcastreceivers/BootAlarmReceiver.java
@@ -1,14 +1,23 @@
 package com.ninise.notereminder.notification.broadcastreceivers;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
 
-public class BootAlarmReceiver extends BroadcastReceiver {
+import com.ninise.notereminder.notification.service.BootAlarmService;
+
+public class BootAlarmReceiver extends WakefulBroadcastReceiver{
+
+    private static final String TAG = "BootAlarmReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Intent service = new Intent(context, BootAlarmService.class);
 
+        Log.i(TAG, "Starting service @ " + SystemClock.elapsedRealtime());
+        startWakefulService(context, service);
     }
 
 }
