61db57340b13a80e48fd0e10c1cef9cb50c4ade0
Ninise <nikitonze@hotmail.com>
2016-02-09 18:46:09 +0200

    Now reminders works after rebooting device

--- app/src/main/java/com/ninise/notereminder/notification/service/BootAlarmService.java
+++ app/src/main/java/com/ninise/notereminder/notification/service/BootAlarmService.java
@@ -5,7 +5,13 @@ import android.content.Intent;
 import android.os.SystemClock;
 import android.util.Log;
 
+import com.ninise.notereminder.database.NoteModel;
+import com.ninise.notereminder.database.NoteWorker;
+import com.ninise.notereminder.notification.alarm.AlarmNotification;
 import com.ninise.notereminder.notification.broadcastreceivers.BootAlarmReceiver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BootAlarmService extends IntentService {
 
@@ -17,6 +23,14 @@ public class BootAlarmService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        List<NoteModel> noteList = new NoteWorker(getApplicationContext()).getAllReminders();
+
+        for (NoteModel note: noteList) {
+            if (note.getTime() >= System.currentTimeMillis()) {
+                new AlarmNotification(getApplicationContext()).setOnceAlarm(note.getTime(), note);
+            }
+        }
+
         Log.i(TAG, "Completed service @ " + SystemClock.elapsedRealtime());
         BootAlarmReceiver.completeWakefulIntent(intent);
     }
