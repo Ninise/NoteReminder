5a7c31556fa8175ab3c65a9c9545492de6b397d5
Ninise <nikitonze@hotmail.com>
2016-02-10 18:59:23 +0200

    Added clause in onBackPressed for NoteActivity

--- app/src/main/java/com/ninise/notereminder/notedata/NoteActivity.java
+++ app/src/main/java/com/ninise/notereminder/notedata/NoteActivity.java
@@ -1,12 +1,18 @@
 package com.ninise.notereminder.notedata;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityManagerCompat;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.ninise.notereminder.R;
+import com.ninise.notereminder.main.MainActivity;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -40,7 +46,17 @@ public class NoteActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+
+        /** Its called when user type on notification */
+        if (this.isTaskRoot()) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+        } else {
+            super.onBackPressed();
+            overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+        }
+
     }
 }
